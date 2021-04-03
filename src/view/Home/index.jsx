02039d1aeb6febae06133bd248dc3ea68f327b33
DevMiguell <buguel2000@gmail.com>
2021-04-03 09:17:41 -0300

    criando-e-renderizando-eventos

--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-import 'firebase/auth'
+import firebase from '../../config/firebase';
 
 import './home.css'
+
 import Navbar from '../../components/navbar'
+import EventoCard from '../../components/evento-card'
 
 function Home() {
+
+    const [eventos, setEventos] = useState([]);
+    let listaeventos = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').get().then(async (resultado) => {
+            await resultado.docs.forEach(doc => {
+                listaeventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setEventos(listaeventos);
+        })
+    })
+
     return (
         <>
             <Navbar />
-            <h1>Pagina Inicial</h1>
+            <h1>{useSelector(state => state.usuarioEmail)}</h1>
+            <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
+
+            <div className="row p-3">
+                {eventos.map(item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} />)}
+            </div>
         </>
     )
 }
